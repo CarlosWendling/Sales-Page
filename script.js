19f60ea1a12897f3910dd618882c72dfa62dee33
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-09-18 14:46:05 -0300

    create products cards

---
+++ script.js
@@ -0,0 +1,18 @@
+let divContainer = document.querySelector('.container')
+
+fetch("dados.json").then((response) => {
+    response.json().then((dados) => {
+        dados.products.map((product) => {
+            divContainer.innerHTML += 
+            `
+            <div class="card">
+                <img src="${product.image}" class="product-img" alt="A ${product.name} photo.">
+                <div class="card-body">
+                    <h5 class="product-name">${product.name}</h5>
+                    <p class="product-description">${product.description}</p>
+                </div>
+            </div>
+            `
+        })
+    })
+})
